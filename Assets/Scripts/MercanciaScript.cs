5f5056e0d658ee401dcd604f63bc7de5f5b28303
Saikokyler <167241122+Saikokyler@users.noreply.github.com>
2025-05-28 15:49:12 -0300

    parkour hecho

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject);
+
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
