92193d879345e2694b8bcdf6d61eb152619468fe
Saikokyler <167241122+Saikokyler@users.noreply.github.com>
2025-05-28 15:25:27 -0300

    nueva scene y organizado el parkopur

---
+++ Assets/Scripts/DeteccionDeColisiones.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeColisiones : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto Con " + collision.gameObject.name);
+        Destroy(collision.gameObject);
+
+    }
+}
