7e931d17d094957996e3ed25d29ea40052e10a86
Saikokyler <167241122+Saikokyler@users.noreply.github.com>
2025-06-04 15:03:13 -0300

    dfghjk

---
+++ Assets/Scripts/MONEYMANAGER.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MONEYMANAGER : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+
+    {
+        if (money + amount < 0)
+        {
+            Debug.Log("No te alcanza");
+        }
+        else
+        { 
+            money += amount;
+        }
+    }
+}
